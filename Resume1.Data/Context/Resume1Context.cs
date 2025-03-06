5fddc9af837590bd9c98e77d18c168aac964b04f
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2025-03-06 18:18:58 +0330

    add package and make relation between model and contex

---
+++ Resume1.Data/Context/Resume1Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Resume1.domain.Models.Auth;
+
+//using Resume1.Domain
+using Resume1.Data.Context;
+
+namespace Resume1.Data.Context
+{
+    public class Resume1Context:DbContext
+    {
+        public Resume1Context(DbContextOptions<Resume1Context> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
+ 
---
+++ Resume1.domain/Models/Auth/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resume1.domain.Models.Auth
+{
+    public class User
+     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FullName { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+    }
+}
