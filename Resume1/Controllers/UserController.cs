da95a2d368fd68c9df3df9bf9616b6bcaf5df2e7
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2025-03-08 22:10:00 +0330

    First view

---
+++ Resume1/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume1.core.Services.Implementation;
+using Resume1.core.Services.Interfaces;
+using Resume1.domain.Models.Auth;
+
+namespace Resume1.Controllers
+{
+    public class UserController: Controller
+    {
+        private IUserService  userService;
+
+        public UserController(IUserService _userService)
+        {
+                userService = _userService;
+        }
+        public ActionResult ShowUser()
+        {
+            List<User> users=userService.GetUsers();
+            return View(users);
+        }
+      
+    }
+}
